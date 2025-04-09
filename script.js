5c982579e6e991728335c80c58010b8f1063a197
Bhavitha <chinthabhavitha6@gmail.com>
2025-04-10 01:38:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.getElementById("signupForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Form Submitted Successfully!");
+});
+document.querySelector(".login-page").addEventListener('click', function() {
+    window.location.href = 'file:///C:/Users/MRCE%20DIGITAL.LIB/Desktop/login%20page/index.html'; // Replace 'another-page.html' with the path to the page you want to navigate to
+});
+document.querySelector(".SignUp").addEventListener('click', function() {
+    window.location.href = 'file:///C:/Users/MRCE%20DIGITAL.LIB/Desktop/health24/Medical-healthcare-website-template-main/index.html#7'; // Replace 'another-page.html' with the path to the page you want to navigate to
+});
